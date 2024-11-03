15309d27784cc56bbc92612849a2a52de6c0faec
d13g0hornburg <diegohornburg@gmail.com>
2024-11-02 21:55:17 -0300

    Refactor navigation in ActionCards component

--- src/Components/ActionCards/ActionCards.js
+++ src/Components/ActionCards/ActionCards.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaPlusCircle, FaListAlt } from 'react-icons/fa';
 import './ActionCards.css';
 
 const ActionCards = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="action-cards-container text-center mt-5">
       <div className="action-cards-row">
         <div className="mb-4">
           <div className="action-card">
-            <button className="icon-button" onClick={() => window.location.href='/addItem'}>
+            <button className="icon-button" onClick={() => navigate('/addItem')}>
               <FaPlusCircle size={50} />
             </button>
           </div>
         </div>
         <div className="mb-4">
           <div className="action-card">
-            <button className="icon-button" onClick={() => window.location.href='/listar-todos'}>
+            <button className="icon-button" onClick={() => navigate('/listar-todos')}>
               <FaListAlt size={50} />
             </button>
           </div>
