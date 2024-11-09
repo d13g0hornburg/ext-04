23a2baa2f72eddd0d15bc84acf77ebbfef56456f
d13g0hornburg <diegohornburg@gmail.com>
2024-11-09 10:54:46 -0300

    Update apoio.css and apoio.js with support for displaying institutions that we support

--- src/Pages/Apoio/apoio.js
+++ src/Pages/Apoio/apoio.js
@@ -1,21 +1,54 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import Helmet from 'react-helmet';
 import Header from '../../Components/Header/header';
 import Footer from '../../Components/Footer/footer';
-import { Helmet } from 'react-helmet';
+import './apoio.css';
+import logo1 from '../../assets/images/logo1.jpg';
+import logo2 from '../../assets/images/logo2.jpg';
+import logo3 from '../../assets/images/logo3.jpg';
+
 
 const Apoio = () => {
-  useEffect(() => {
-    document.title = 'Apoio'; 
-  }, []); 
+  const institutions = [
+    {
+      name: 'Instituição 1',
+      logo: logo1,
+      description: 'Descrição da Instituição 1.',
+      contact: 'contato@instituicao1.com',
+    },
+    {
+      name: 'Instituição 2',
+      logo: logo2,
+      description: 'Descrição da Instituição 2.',
+      contact: 'contato@instituicao2.com',
+    },
+    {
+      name: 'Instituição 3',
+      logo: logo3,
+      description: 'Descrição da Instituição 3.',
+      contact: 'contato@instituicao3.com',
+    },
+  ];
 
   return (
     <div>
-       <Helmet>
-      <title>Apoio</title>
-    </Helmet>
+      <Helmet>
+        <title>Apoio</title>
+      </Helmet>
       <Header />
-      <div className="container mt-5">
-      </div>
+      <main id="mainApoio">
+        <h1>Instituições que Apoiamos</h1>
+        <div className="institutions">
+          {institutions.map((institution, index) => (
+            <div key={index} className="institution">
+              <img src={institution.logo} alt={`${institution.name} logo`} className="institution-logo" />
+              <h2>{institution.name}</h2>
+              <p>{institution.description}</p>
+              <p><b>Contato:</b> {institution.contact}</p>
+            </div>
+          ))}
+        </div>
+      </main>
       <Footer />
     </div>
   );
