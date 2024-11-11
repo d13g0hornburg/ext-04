7974ce6f9efd9fe91e501e11afe23404cf5ab417
mateuspadilha166 <mateuspadilha873@gmail.com>
2024-11-11 20:14:22 -0300

    Adicionado botchat que faz um link para o telegram

--- server.js
+++ server.js
@@ -13,8 +13,28 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'src/uploads')));
 
 const storage = multer.memoryStorage(); // Armazena a imagem na memória temporariamente
-
+const TELEGRAM_BOT_LINK = 'https://web.telegram.org/a/#8001707439';
 const upload = multer({ storage });
+
+app.post('/chatbot', (req, res) => {
+  console.log('Mensagem recebida:', req.body.message);
+  const { message } = req.body;
+
+  let botResponse = '';
+
+  if (message.toLowerCase().includes('olá')) {
+    botResponse = 'Olá! Como posso ajudar você hoje?';
+  } else if (message.toLowerCase().includes('achar um item específico')) {
+    // Resposta com link clicável
+    botResponse = 'Para isso, entre em contato com a gente no Telegram: <a href="https://web.telegram.org/a/#8001707439" target="_blank">Clique aqui para acessar</a>';
+  } else {
+    botResponse = 'Desculpe, não entendi. Poderia reformular?';
+  }
+
+  res.json({ message: botResponse });
+});
+
+
 
 app.post('/upload', upload.single('imagem_objeto'), async (req, res) => {
   if (!req.file) {
